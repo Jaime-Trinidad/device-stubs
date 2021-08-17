893ebfc0889df5a985ddcfb7e705eacc48253811
Jaime Trinidad <81958808+Jaime-Trinidad@users.noreply.github.com>
2021-08-17 10:59:11 -0500

    update
    update

--- src/generate.js
+++ src/generate.js
@@ -18,7 +18,7 @@ if(options.template === 'index'){
             {question: chalk.dim( 'Unique identifier of the end device (lowercase, alphanumeric with dashes, max 36 characters)'), slot: '__id__'}
             ],
             output: {
-                path: './device/__index__(lowerCase).yaml',
+                path: './new_device/__index__(lowerCase).yaml',
                 pathAndFileNameDefaultCase: '(kebabCase)',
             },
         }, 
@@ -33,14 +33,14 @@ if(options.template === 'index'){
                 folderPath: './__model__.yaml',
             },
             stringReplacers: [
-                {question: chalk.green.bold('File name'), slot: '__model__'},
+                {question: chalk.green.bold('Write your file name'), slot: '__model__'},
                 {question: chalk.dim('Name of your End Device'), slot: '__name__'},
                 {question: chalk.dim('Add a description of your End Device'), slot: '__description__'}, 
                 {question: chalk.dim('Hardware Version?'), slot: '__hwdversion__'},
                 {question: chalk.dim('Firmware Version?'), slot: '__fmwversion__'}, 
                 { question: chalk.dim('Your device is LoRaWAN Certified? (true or false)'), slot: '__loracert__'}],
             output: {
-                path: './device/__model__(lowerCase).yaml',
+                path: './new_device/__model__(lowerCase).yaml',
                 pathAndFileNameDefaultCase: '(kebabCase)',
             },
         }, 
@@ -55,12 +55,12 @@ if(options.template === 'index'){
                 folderPath: './__profile__-profile.yaml',
             },
             stringReplacers: [
-                {question: chalk.green.bold('Name of your profile file'), slot: '__profile__'},
+                {question: chalk.green.bold('Write your file name (ex. eu868, us915,...)'), slot: '__profile__'},
                 {question: chalk.dim('LoRaWAN MAC version: 1.0, 1.0.1, 1.0.2, 1.0.3, 1.0.4 or 1.1'), slot: '__macversion__'},
                 {question: chalk.dim('Parameters Version: 1.0: TS001-1.0, 1.0.1: TS001-1.0.1, 1.0.2: RP001-1.0.2 or RP001-1.0.2-RevB, 1.0.3: RP001-1.0.3-RevA, 1.0.4: RP002-1.0.0 or RP002-1.0.1, 1.1: RP001-1.1-RevA or RP001-1.1-RevB'), slot: '__paramversion__'}, 
                 {question: chalk.dim('Maximum EIRP'), slot: '__maxeirp__'}],
             output: {
-                path: './device/__profile__(lowerCase)-profile.yaml',
+                path: './new_device/__profile__(lowerCase)-profile.yaml',
                 pathAndFileNameDefaultCase: '(kebabCase)',
             },
         }, 
@@ -75,11 +75,11 @@ if(options.template === 'index'){
                 folderPath: './__codec__-codec.yaml',
             },
             stringReplacers: [
-                {question: chalk.greenBright.bold('Name of your codec file'), slot: '__codec__'},
-                {question: chalk.dim('Name of your decoder file'), slot: '__decoder__'}
+                {question: chalk.greenBright.bold('Write your file name'), slot: '__codec__'},
+                {question: chalk.dim('Name of your decoder file (ex. gnse.js)'), slot: '__decoder__'}
                 ],
             output: {
-                path: './device/__codec__(lowerCase)-codec.yaml',
+                path: './new_device/__codec__(lowerCase)-codec.yaml',
                 pathAndFileNameDefaultCase: '(kebabCase)',
             },
         }, 
